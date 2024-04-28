81d76dfa5a9d28ad06a7e2ecc42892fb0638f770
Santiago Sanchez Pulgarin <santiago.sanchez.ssaa@gmail.com>
2024-04-28 15:31:09 -0500

    fix(app): Change Endpoint logic (breaking change)

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
+import pandas as pd
+from flask import Flask, render_template, request
 
-app = Flask(
-    __name__,
-    template_folder="templates",
-    static_folder="static"
-)
+# from model import loal_model, preprocess_data
 
-@app.route('/')
+app = Flask(__name__)
+
+def predict_stock_price(stock_symbol):
+    return "some_response¿?"
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        # Get the stock symbol from the form
+        stock_symbol = request.form['stock_symbol']
+        
+        # Make a prediction using the trained model
+        prediction = predict_stock_price(stock_symbol)
+        
+        # Render the result template with the prediction
+        return render_template('result.html', prediction=prediction)
+    
     return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
